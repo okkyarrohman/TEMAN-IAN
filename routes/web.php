1f019f5f1161f91d2e90891c86ba237216b9aca4
okkyarrohman <okkyzee@gmail.com>
2023-08-20 12:32:28 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sign-in', function () {
+Route::get('/ss', function () {
+    return view('welcome');
+});
+
+
+
+
+// Route untuk Murid
+Route::get('/sign.in', function () {
     return view('sign-in');
 });
 
-Route::get('/sign-up', function () {
+Route::get('/sign.up', function () {
     return view('sign-up');
 });
+
+// End Route Untuk Murid
+
+
+
+// Route Untuk Guru
+Route::get('/guru-dashboard',function(){
+    return view('guru.dashboard');
+});
+
+Route::get('',function(){
+
+});
+
+
+// End Route Untuk Guru
+
+
+
+// Route Untuk Admin
+Route::get('/admin-dashboard',function(){
+    return view('admin.dashboard');
+});
+
+// End Route Untuk Admin
